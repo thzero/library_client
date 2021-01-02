891d209fb33092069b6b5cac269410196e1d222d
thzero <thzero@users.noreply.github.com>
2021-01-02 16:03:55 -0600

    updates

--- service/utility.js
+++ service/utility.js
@@ -4,6 +4,9 @@ import RestExternalService from './externalRest';
 
 class UtilityService extends RestExternalService {
 	async logger(correlationId, content) {
+		if (!this._serviceCommunicationRest)
+			return this._success(correlationId);
+
 		return await this._serviceCommunicationRest.post(correlationId, LibraryConstants.ExternalKeys.BACKEND, 'utility/logger', content);
 	}
 }
