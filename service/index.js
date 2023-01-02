127cb44a6fe30ab796d3ae14d13d67640b4d12b3
thzero <thzero@users.noreply.github.com>
2023-01-02 10:47:46 -0600

    updates

--- service/index.js
+++ service/index.js
@@ -23,49 +23,70 @@ class Service {
 	_enforce(clazz, method, value, name, correlationId, message) {
 		if (!value) {
 			if (!String.isNullOrEmpty(message))
-				message = `Invalid ${name}.`;
+				message = `${name} is invalid.`;
 
 			this._logger.error(clazz, method, message, null, correlationId);
-			throw Error(message);
+			const error = Error(message, true);
+			error.correlationId = correlationId;
+			throw error;
 		}
 	}
 
 	_enforceNotEmpty(clazz, method, value, name, correlationId) {
 		if (String.isNullOrEmpty(value)) {
 			this._logger.error(clazz, method, `${name} is empty.`, null, correlationId);
-			throw Error(`${name} is empty.`);
+			const error = Error(`${name} is empty.`, true);
+			error.correlationId = correlationId;
+			throw error;
 		}
 	}
 	
 	_enforceNotEmptyEither(clazz, method, value1, value2, name1, name2, correlationId) {
 		if (String.isNullOrEmpty(value1) && String.isNullOrEmpty(value2)) {
 			this._logger.error(clazz, method, `Either ${name1} or ${name2} is empty.`, null, correlationId);
-			throw Error(`Either ${name1} or ${name2} is empty.`);
+			const error = Error(`Either ${name1} or ${name2} is empty.`, true);
+			error.correlationId = correlationId;
+			throw error;
 		}
 	}
 
-	_enforceNotNullMultiple(clazz, method, values, names, correlationId) {
+	_enforceNotEmptyMultiple(clazz, method, values, names, correlationId) {
 		const valid  = true;
 		for (const value of values)
 			valid &= String.isNullOrEmpty(value);
 		if (!valid) {
 			names = names.join(', ');
 			this._logger.error(clazz, method, `None of the fields are not null: ${names}`, null, correlationId);
-			throw Error(`None of the fields are not null: ${names}`);
+			const error = Error(`None of the fields are not null: ${names}`, true);
+			error.correlationId = correlationId;
+			throw error;
 		}
+	}
+
+	_enforceNotEmptyResponse(clazz, method, value, name, correlationId) {
+		if (String.isNullOrEmpty(value)) {
+			this._logger.error(clazz, method, `${name} is empty.`, null, correlationId);
+			return Response.error(clazz, method, `${name} is empty.`, null, null, null, correlationId);
+		}
+
+		return this._successResponse(value, correlationId);
 	}
 
 	_enforceNotNull(clazz, method, value, name, correlationId) {
 		if (!value || value === undefined) {
 			this._logger.error(clazz, method, `${name} is null.`, null, correlationId);
-			throw Error(`${name} is null.`);
+			const error = Error(`${name} is null.`, true);
+			error.correlationId = correlationId;
+			throw error;
 		}
 	}
 
 	_enforceNotNullEither(clazz, method, value1, value2, name1, name2, correlationId) {
 		if ((!value1 || value1 === undefined) && (!value2 || value2 == undefined)) {
 			this._logger.error(clazz, method, `Either ${name1} or ${name2} is null.`, null, correlationId);
-			throw Error(`Either ${name1} or ${name2} is null.`);
+			const error = Error(`Either ${name1} or ${name2} is null.`, true);
+			error.correlationId = correlationId;
+			throw error;
 		}
 	}
 
@@ -76,55 +97,31 @@ class Service {
 		if (!valid) {
 			names = names.join(', ');
 			this._logger.error(clazz, method, `None of the fields are not null: ${names}`, null, correlationId);
-			throw Error(`None of the fields are not null: ${names}`);
+			const error = Error(`None of the fields are not null: ${names}`, true);
+			error.correlationId = correlationId;
+			throw error;
 		}
 	}
 
 	_enforceNotNullResponse(clazz, method, value, name, correlationId) {
-		if (!value) {
-			this._logger.error(clazz, method, `${name} is null.`, null, correlationId);
-			return Response.error(`${name} is null.`, null);
-		}
-
-		return this._success(correlationId);
-	}
-
-	_enforceNotEmptyResponse(clazz, method, value, name, correlationId) {
-		if (String.isNullOrEmpty(value)) {
-			this._logger.error(clazz, method, `${name} is empty.`, null, correlationId);
-			return Response.error(`${name} is empty.`, null);
-		}
-
-		return this._success(correlationId);
-	}
-
-	_enforceNotNullAsResponse(clazz, method, value, name, correlationId) {
 		if (!value || value === undefined) {
 			this._logger.error(clazz, method, `${name} is null.`, null, correlationId);
-			return Response.error(`${name} is null.`, null);
+			return Response.error(clazz, method, `${name} is null.`, null, null, null, correlationId);
 		}
 
-		const response = this._initResponse(correlationId);
-		response.results = value;
-		return response;
+		return this._successResponse(value, correlationId);
 	}
 
-	_enforceNotEmptyAsResponse(clazz, method, value, name, correlationId) {
-		if (String.isNullOrEmpty(value)) {
-			this._logger.error(clazz, method, `${name} is empty.`, null, correlationId);
-			return Response.error(`${name} is empty.`, null);
+	_enforceResponse(clazz, method, response, name, correlationId, message) {
+		if (!response || (response && !response.success)) {
+			if (!String.isNullOrEmpty(message))
+				message = `Unsuccessful response for ${name}.`;
+
+			this._logger.error(clazz, method, message, null, correlationId);
+			const error = Error(message, true);
+			error.correlationId = correlationId;
+			throw error;
 		}
-
-		const response = this._initResponse(correlationId);
-		response.results = value;
-		return response;
-	}
-
-	_enforceResponse(clazz, method, response, name, correlationId) {
-		if (!response || (response && !response.success))
-			return false;
-
-		return true;
 	}
 
 	_error(clazz, method, message, err, code, errors, correlationId) {
