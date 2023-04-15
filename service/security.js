e33bfc12a99a1caf3113e808e2f9de7395c0158a
thzero <thzero@users.noreply.github.com>
2023-04-15 12:59:14 -0500

    updates

--- service/security.js
+++ service/security.js
@@ -30,6 +30,98 @@ class SecurityService extends Service {
 		this._enforcers.set(key, enforcer);
 	}
 
+	async authorizationCheckClaims(correlationId, claims, roles, logical) {
+		if (!claims)
+			return false;
+		if (!(claims && Array.isArray(claims)))
+			return false;
+		if (!roles)
+			return true;
+
+		if (String.isNullOrEmpty(logical) || (logical !== SecurityService.logicalAnd) || (logical !== SecurityService.logicalOr))
+			logical = SecurityService.logicalOr;
+
+		let success = (logical === SecurityService.logicalOr ? false : true);
+
+		let result;
+		let roleAct;
+		let roleObj;
+		let roleParts;
+		for (const claim of claims) {
+			this._serviceLogger.debug('SecurityService', 'authorizationCheckClaims', 'authorization.claim', claim, correlationId);
+
+			for (const role of roles) {
+				this._serviceLogger.debug('SecurityService', 'authorizationCheckClaims', 'role', role, correlationId);
+
+				roleParts = role.split('.');
+				if (roleParts && roleParts.length < 1)
+					success = false;
+
+				roleObj = roleParts[0];
+				roleAct = roleParts.length >= 2 ? roleParts[1] : null
+
+				result = await this._serviceSecurity.validate(claim, null, roleObj, roleAct);
+				this._serviceLogger.debug('SecurityService', 'authorizationCheckClaims', 'result', result, correlationId);
+				if (logical === SecurityService.logicalOr)
+					success = success || result;
+				else
+					success = success && result;
+			}
+		}
+
+		return success;
+	}
+
+	async authorizationCheckRoles(correlationId, user, roles, logical) {
+		if (!user)
+			return false;
+		if (!roles || !Array.isArray(roles) || (roles.length === 0))
+			return true;
+
+		this._logger.debug('SecurityService', 'authorizationCheckRoles', 'user', user, correlationId);
+		if (!(user && user.roles && Array.isArray(user.roles)))
+			return false;
+
+		if (String.isNullOrEmpty(logical) || (logical !== SecurityService.logicalAnd) || (logical !== SecurityService.logicalOr))
+			logical = SecurityService.logicalOr;
+
+		let success = (logical === SecurityService.logicalOr ? false : true);
+
+		this._logger.debug('SecurityService', 'authorizationCheckRoles', 'logical', logical, correlationId);
+
+		let result;
+		let roleAct;
+		let roleObj;
+		let roleParts;
+		for (const userRole of user.roles) {
+			this._logger.debug('SecurityService', 'authorizationCheckRoles', 'userRole', userRole, correlationId);
+
+			for (const role of roles) {
+				this._logger.debug('SecurityService', 'authorizationCheckRoles', 'role', role, correlationId);
+
+				roleParts = role.split('.');
+				if (roleParts && roleParts.length < 1)
+					success = false;
+
+				roleObj = roleParts[0];
+				roleAct = roleParts.length >= 2 ? roleParts[1] : null
+
+				result = await this.validate(correlationId, userRole, null, roleObj, roleAct);
+				this._logger.debug('SecurityService', 'authorizationCheckRoles', 'result', result, correlationId);
+				if (logical === SecurityService.logicalOr) {
+					if (result)
+						return result;
+
+					success = false;
+				}
+				else
+					success = success && result;
+			}
+		}
+
+		return success;
+	}
+
 	async validate(correlationId, sub, dom, obj, act) {
 		return this.validateEx(correlationId, KeyEnforcerDefault, sub, dom, obj, act);
 	}
@@ -58,6 +150,9 @@ class SecurityService extends Service {
 	_initModel() {
 		return null;
 	}
+
+	static logicalAnd = 'and';
+	static logicalOr = 'or';
 }
 
 export default SecurityService;
