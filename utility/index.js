1a3b1888b413a4630b012296b2ea92a3c7bee9a5
thzero <thzero@users.noreply.github.com>
2023-12-28 09:50:13 -0600

    updates for new utilities

--- utility/index.js
+++ utility/index.js
@@ -1,4 +1,17 @@
+import LibraryCommonUtility from '@thzero/library_common/utility';
+
 class LibraryClientUtility {
+	static isDebug = false;
+
+	static convertNumber(value, defaultValue) {
+		defaultValue = defaultValue ? defaultValue : null;
+
+		if (String.isNullOrEmpty(value))
+			return null;
+		value = String.trim(value);
+		return !String.isNullOrEmpty(value) ? Number(value) : defaultValue;
+	}
+
 	static dateFormat(locale) {
 		locale = !String.isNullOrEmpty(locale) ? locale : LibraryClientUtility.getLocale();
 		const formatObj = new Intl.DateTimeFormat(locale).formatToParts(new Date());
@@ -16,6 +29,25 @@ class LibraryClientUtility {
 		}).join('');
 	}
 
+	static debug(args) {
+		if (!LibraryClientUtility.isDebug)
+			return;
+		console.debug(args);
+	}
+
+	static debug2(name, value) {
+		if (!LibraryClientUtility.isDebug)
+			return;
+
+		// eslint-disable-next-line no-unneeded-ternary
+		const output = name + ': ' + (value ? value : 'null');
+		console.debug(output);
+	}
+
+	static info(args) {
+		console.info(args);
+	}
+
 	static getLocale() {
 		return (navigator.languages && navigator.languages.length) ? navigator.languages[0] : navigator.language;
 	}
@@ -26,6 +58,21 @@ class LibraryClientUtility {
 
 	static set online(value) {
 		LibraryClientUtility._online = value;
+	}
+
+	static userDisplayName(correlationId, user) {
+		if (!user || !user.settings)
+			return '';
+
+		const settings = user.settings ? user.settings : null;
+		const userName = settings && settings.gamerTag ? settings.gamerTag : user.external && user.external.name ? user.external.name : '******';
+		return userName;
+	}
+
+	static ttlDelta(ttl) {
+		const now = LibraryCommonUtility.getTimestamp();
+		const delta = ttl - now;
+		return delta > 0;
 	}
 	
 	static _online = false;
